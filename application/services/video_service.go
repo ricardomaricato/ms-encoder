3793add959bc1950ec9d7bf2820083578ef7783a
Ricardo Maricato <ricardo.maricato@hotmail.com>
2021-08-31 23:09:30 -0300

    -Fragmenting video

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -7,6 +7,7 @@ import (
 	"ms-encoder/application/repositories"
 	"ms-encoder/domain"
 	"os"
+	"os/exec"
 
 	"cloud.google.com/go/storage"
 )
@@ -58,4 +59,31 @@ func (v *VideoService) Download(bucketName string) error {
 	log.Printf("video %v has been stored", v.Video.ID)
 
 	return nil
+}
+
+func (v *VideoService) Fragment() error {
+
+	err := os.Mkdir(os.Getenv("localStoragePath")+"/"+v.Video.ID, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	source := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".mp4"
+	target := os.Getenv("localStoragePath") + "/" + v.Video.ID + ".frag"
+
+	cmd := exec.Command("mp4fragment", source, target)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	printOutput(output)
+
+	return nil
+}
+
+func printOutput(out []byte) {
+	if len(out) > 0 {
+		log.Printf("=====> Output: %s\n", string(out))
+	}
 }
